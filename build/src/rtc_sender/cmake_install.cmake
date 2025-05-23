dd0ab72d6ca853a027399cf7bba020e4ff62099c
gabrielt17 <gabriel.muniz@ufv.br>
2025-05-23 14:38:14 -0300

    Created a ubuntu branch for compiling code in NERo

--- build/src/rtc_sender/cmake_install.cmake
+++ build/src/rtc_sender/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/gabrielt/Documentos/Códigos/webrtc_ws/src/rtc_sender
+# Install script for directory: /home/gabrielt/Documentos/Códigos/WebRTC-Media-Sender-Receiver/src/rtc_sender
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -29,7 +29,7 @@ endif()
 
 # Install shared libraries without execute permission?
 if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "0")
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
 endif()
 
 # Is this installation the result of a crosscompile?
@@ -37,14 +37,8 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-# Set path to fallback-tool for dependency-resolution.
+# Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+  set(CMAKE_OBJDUMP "/usr/bin/llvm-objdump-19")
 endif()
 
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/home/gabrielt/Documentos/Códigos/webrtc_ws/build/src/rtc_sender/install_local_manifest.txt"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
-endif()
